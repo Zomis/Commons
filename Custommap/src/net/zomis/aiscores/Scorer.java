a826a2089f3fcbdeff0328f5be43f1ca7002ee3a
Simon Forsberg <zomis2k@hotmail.com>
2014-02-01 18:30:15 +0100

    Extracted AIScorers and Fighting to their own repositories

--- Custommap/src/net/zomis/aiscores/Scorer.java
+++
@@ -1,6 +0,0 @@
-package net.zomis.aiscores;
-
-/**
- * Marker for classes that are a part of scoring things, i.e. {@link PostScorer} and {@link AbstractScorer}.
- */
-public interface Scorer { }
--- Custommap/src/net/zomis/fight/FightInterface.java
+++
@@ -1,7 +0,0 @@
-package net.zomis.fight;
-
-public interface FightInterface<PL> {
-	int	FIRST_FIGHT	= 1;
-
-	PL determineWinner(PL[] players, int fightNumber);
-}
